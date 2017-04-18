cf998e3979f5830d37d38f35ec270ba343dc606c
Harris Leshaba <u15312144@tuks.co.za>
2017-04-18 17:47:00 +0200

    Update ClientLoginWeb.java

--- ClientLoginWeb.java
+++ ClientLoginWeb.java
@@ -23,14 +23,18 @@ public class ClientLoginWeb {
       webClient.getOptions().setUseInsecureSSL(true);  //bypass certificate validation
       HtmlPage page1 = webClient.getPage("https://137.215.6.208");
       HtmlForm form = page1.getHtmlElementById("login-form");
- 
+      //logging in
       HtmlSubmitInput submitButton = form.getInputByValue("Log In");
       HtmlTextInput textFieldEmail = form.getInputByName("j_username");
       textFieldEmail.setValueAttribute("admin");
       HtmlPasswordInput textFieldPass = form.getInputByName("j_password");
       textFieldPass.setValueAttribute("Aruba123!");
-      HtmlPage page2 = submitButton.click();
+      HtmlPage dashboard = submitButton.click();
       
+       //Creating request to query aruba for location
+      URL url= new URL("https://137.215.6.208/api/v1/location?sta_eth_mac=48:9D:24:B9:41:CB");
+      WebRequest locationReq = new WebRequest(url);
+      WebResponse page2 =  webClient.loadWebResponse(locationReq); 
       System.out.println(page2);
     }
 }
