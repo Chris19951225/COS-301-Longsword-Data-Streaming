ad33618b967d34bd20afe5d87693ab4b7c42aae6
Ritesh Doolabh <ritzd93@gmail.com>
2017-04-20 12:59:12 +0200

    Update Data2.java

--- FINAL/Data2.java
+++ FINAL/Data2.java
@@ -84,9 +84,6 @@ public class Data2 {
             floor_id = location.getString("floor_id");
             hashed_sta_eth_mac = location.getString("hashed_sta_eth_mac");
             loc_algorithm = location.getString("loc_algorithm");
-            longitude = location.getDouble("longitude");
-            latitude =location.getDouble("latitude");
-            altitude = location.getDouble("altitude");
             unit = location.getString("unit");
 
             String clean = "";
