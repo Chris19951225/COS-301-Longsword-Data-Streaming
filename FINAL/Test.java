3500be71592b7c079fc90889bc51f31da1010fc8
Ritesh Doolabh <ritzd93@gmail.com>
2017-04-20 13:00:55 +0200

    Update Test.java

--- FINAL/Test.java
+++ FINAL/Test.java
@@ -1,12 +1,10 @@
 package org.apache.flink;
 
 
-/**
- * Created by Ritz on 2017/04/20.
- */
 public class Test {
 
     public static void main(String[] args) throws Exception {
+        //Creates a Data2 object and passes a mac address through then prints the output
         Data2 data2obj= new Data2();
         System.out.println("beginning test");
         org.json.JSONObject obj = data2obj.getLocation("48:5A:3F:79:A3:D1");
