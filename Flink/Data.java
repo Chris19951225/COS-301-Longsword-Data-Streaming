048c06e31f9f0f12dc08ee81f750b9d4b663c5b7
Lucian Sargeant <luciansargeant@gmail.com>
2017-04-21 21:29:26 +0200

    Update Data.java
    Final Changes

--- Flink/Data.java
+++ Flink/Data.java
@@ -1,16 +1,5 @@
 package org.apache.flink;
 
-// Other imports
-import java.net.URL;
-import org.apache.http.client.CredentialsProvider;
-import org.apache.http.NoHttpResponseException;
-import org.apache.http.entity.mime.content.ContentBody;
-import net.sourceforge.htmlunit.corejs.javascript.ContextAction;
-import org.w3c.css.sac.ErrorHandler;
-import org.w3c.dom.ElementTraversal;
-import org.w3c.*;
-import org.apache.flink.api.common.functions.MapFunction;
-
 /*
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
@@ -29,43 +18,17 @@ import org.apache.flink.api.common.functions.MapFunction;
  * limitations under the License.
  */
 
+// Flink imports
+import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.sink.SocketClientSink;
 import org.apache.flink.streaming.util.serialization.SimpleStringSchema;
-import org.apache.log4j.BasicConfigurator;
-import org.json.JSONArray;
 
-// Gargoyle imports
-import com.gargoylesoftware.htmlunit.WebClient;
-import com.gargoylesoftware.htmlunit.WebRequest;
-import com.gargoylesoftware.htmlunit.WebResponse;
-import com.gargoylesoftware.htmlunit.html.HtmlForm;
-import com.gargoylesoftware.htmlunit.html.HtmlPage;
-import com.gargoylesoftware.htmlunit.html.HtmlPasswordInput;
-import com.gargoylesoftware.htmlunit.html.HtmlSubmitInput;
-import com.gargoylesoftware.htmlunit.html.HtmlTextInput;
-
-@SuppressWarnings("unused")
 public class Data
 {
-    @SuppressWarnings("resource")
-	public static void main(String[] args) throws Exception
+    public static void main(String[] args) throws Exception
     {
-        // Logging in to Aruba
-        BasicConfigurator.configure();
-      
-        WebClient webClient = new WebClient();
-        webClient.getOptions().setUseInsecureSSL(true);  //bypass certificate validation
-        HtmlPage page1 = webClient.getPage("https://137.215.6.208");
-        HtmlForm form = page1.getHtmlElementById("login-form");
-        HtmlSubmitInput submitButton = form.getInputByValue("Log In");
-        HtmlTextInput textFieldEmail = form.getInputByName("j_username");
-        textFieldEmail.setValueAttribute("admin");
-        HtmlPasswordInput textFieldPass = form.getInputByName("j_password");
-        textFieldPass.setValueAttribute("Aruba123!");
-        HtmlPage dashboard = submitButton.click();
-
         // The host and the port to connect to
         final String hostname = "localhost";
         final int port = 9000;
@@ -80,41 +43,14 @@ public class Data
         // Create a data stream to receive text via a socket
         DataStream<String> text = env.socketTextStream(hostname, port, "\n");
 
-        DataStream<String> locationRequests = text.map(new MapFunction<String, String>() {
+
+        //
+        DataStream<String> locationRequests = text
+            .map(new MapFunction<String, String>() {
             public String map(String value)
             {
-                // Creating request to query aruba for location
-                WebResponse response = null;
-                URL url=null;
-                WebRequest locationReq =null;
-                
-                try
-                {
-                	if(url==null)
-                		url= new URL("https://137.215.6.208/api/v1/location?sta_eth_mac=" + value);
-                    if(locationReq==null)
-                    	locationReq = new WebRequest(url);
-                    if(response==null)
-                    	response =  webClient.loadWebResponse(locationReq);
-                }
-                catch(Exception e)
-                {
-                    System.out.println("An exception occurred while creating a URL request object.");
-                }
-
-                // Sanitise the data received from Aruba
-                org.json.JSONObject obj = null;
-                if(response != null)
-                {
-                    obj = filterArubaReturn(response.getContentAsString());
-                }
-                else
-                {
-                    System.out.println("Error. No response received.");
-                }
-
                 // Return the sanitised JSON data as a string that can be parsed upon being received
-                return (obj.toString() + "\n");
+                return (value + "\n");
             }
         });
 
@@ -124,76 +60,8 @@ public class Data
 
        // Triggers the program execution. The environment will execute all parts of the program that have resulted in a "sink" operation.
        env.execute("Data");
-       webClient.close();
-       
     }
-
 
     // ------------------------------------------------------------------------
-
-    public static org.json.JSONObject filterArubaReturn(String json)
-    {
-        //Just a tester json object, real one should be passed as the string parameter
-
-        org.json.JSONObject unclean = null;
-        org.json.JSONObject cleaned = null;
-
-        try
-        {
-            unclean = new org.json.JSONObject(json);
-
-            //the bellow can later be removed
-            String addr = null;
-            double sta_location_x = 0, sta_location_y = 0;
-            int error_level = 0;
-            boolean associated = false;
-            String campus_id = null, building_id = null, floor_id = null, hashed_sta_eth_mac = null;
-            String loc_algorithm = null;
-            double longitude = 0, latitude = 0, altitude = 0;
-            String unit = null;
-
-            JSONArray inner = unclean.getJSONArray("Location_result");
-            org.json.JSONObject pos = inner.getJSONObject(0);
-            org.json.JSONObject location = pos.getJSONObject("msg");
-            org.json.JSONObject address = location.getJSONObject("sta_eth_mac");
-
-
-            //Things integration doesn't want can be removed later
-            addr = address.getString("addr");
-            sta_location_x = location.getDouble("sta_location_x");
-            sta_location_y = location.getDouble("sta_location_y");
-            error_level = location.getInt("error_level");
-            associated = location.getBoolean("associated");
-            campus_id = location.getString("campus_id");
-            floor_id = location.getString("floor_id");
-            hashed_sta_eth_mac = location.getString("hashed_sta_eth_mac");
-            loc_algorithm = location.getString("loc_algorithm");
-            unit = location.getString("unit");
-
-            String clean = "";
-            clean += "{\n   \"Location\": {\n";
-            clean += "      \"addr\": \"" + addr + "\",\n";
-            clean += "      \"sta_location_x\": " + sta_location_x + ",\n";
-            clean += "      \"sta_location_y\": " + sta_location_y + ",\n";
-            clean += "      \"error_level\": " + error_level + ",\n";
-            clean += "      \"associated\": " + associated + ",\n";
-            clean += "      \"campus_id\": \"" + campus_id + "\",\n";
-            clean += "      \"building_id\": \"" + building_id + "\",\n";
-            clean += "      \"floor_id\": \"" + floor_id + "\",\n";
-            clean += "      \"hashed_sta_eth_mac\": \"" + hashed_sta_eth_mac + "\",\n";
-            clean += "      \"loc_algorithm\": \"" + loc_algorithm + "\",\n";
-            clean += "      \"unit\": \"" + unit + "\"\n";
-            clean += "  }\n}";
-
-            System.out.println(clean);
-
-            cleaned = new org.json.JSONObject(clean);
-        }
-        catch(Exception e)
-        {
-            System.out.println("Exception occurred while filtering JSON object.");
-        }
-
-        return cleaned;
-    }
 }
+
